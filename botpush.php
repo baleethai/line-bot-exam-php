f6725c1b7158263b59db91636bf40f24f3df4168
Thinnakorn <baleethai@gmail.com>
2018-06-15 12:20:02 +0700

    updated bot

--- botpush.php
+++ botpush.php
@@ -1,20 +1,34 @@
 <?php
 
-
-
 require "vendor/autoload.php";
 
 $access_token = 'bKpDXfpmaoOOpVw3pa550Y86IJEeA3fiLLq6wkvRWGEUfv5aVAYQSNXF+A7t+QvH9Bx5z2GW5UbkwZviwR724oKlMyV8q+gvsr+mBBnD74snEPH6/q8FouU+9KSzi4boTJnAyiymcAhzZqOwvvLj0AdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '0c409b000897d0e406a4c0407b9b1423';
 
-$pushID = 'U28aa0979914c25158af0c800f5d1153c';
+$customers = array(
+	array(
+		'id' => 1,
+		'name' => 'Pak',
+		'pushId' => 'U28aa0979914c25158af0c800f5d1153c' // Pak
+	),
+	array(
+		'id' => 2,
+		'name' => 'Tum',
+		'pushId' => 'U5c1187ba2b5c3fd86adfd3667dd2c3f2' // Tum
+	),
+);
+
+// $pushID = 'U28aa0979914c25158af0c800f5d1153c';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีตำแหน่งงาน PHP Web Developer ใหม่');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+$message = 'มีตำแหน่งงาน PHP Web Developer ใหม่';
+foreach ($customers as $key => $value) {
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+	$response = $bot->pushMessage($value['pushId'], $textMessageBuilder);
+}
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
