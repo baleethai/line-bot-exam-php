4a431151fb620fccad7a6755ce8b235421ab29c8
Thinnakorn <baleethai@gmail.com>
2018-06-16 08:51:06 +0700

    updated webhooks

--- profile.php
+++ profile.php
@@ -1,8 +1,8 @@
 <?php
 
 $access_token = 'bKpDXfpmaoOOpVw3pa550Y86IJEeA3fiLLq6wkvRWGEUfv5aVAYQSNXF+A7t+QvH9Bx5z2GW5UbkwZviwR724oKlMyV8q+gvsr+mBBnD74snEPH6/q8FouU+9KSzi4boTJnAyiymcAhzZqOwvvLj0AdB04t89/1O/w1cDnyilFU=';
-$userId = 'U5c1187ba2b5c3fd86adfd3667dd2c3f2';
 
+$userId = 'U5c1187ba2b5c3fd86adfd3667dd2c3f2';
 $channelSecret = '0c409b000897d0e406a4c0407b9b1423';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
