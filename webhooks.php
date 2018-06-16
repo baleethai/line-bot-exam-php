edc4f0ac964fc161358fe2d8a546004e7569b8d6
Thinnakorn <baleethai@gmail.com>
2018-06-16 09:08:15 +0700

    updated webhooks

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => print_r($event['source'])
+				'text' => print_r(get_class_methods($event['source']))
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
