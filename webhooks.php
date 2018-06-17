7f0f7428d70da197473b6383ceb8afd3b905baa3
Thinnakorn <baleethai@gmail.com>
2018-06-17 12:13:33 +0700

    updated bot

--- webhooks.php
+++ webhooks.php
@@ -39,7 +39,6 @@ if (!is_null($events['events'])) {
 			    $conn = new PDO("mysql:host=$servername;dbname=job_demo", $username, $password);
 			    // set the PDO error mode to exception
 			    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 			    // prepare sql and bind parameters
 			    $stmt = $conn->prepare("INSERT INTO users (user_id, name, position) 
 			    VALUES (:user_id, :name, :position)");
@@ -58,32 +57,33 @@ if (!is_null($events['events'])) {
 			}
 
 			// Get replyToken
-			// $replyToken = $event['replyToken'];
+			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			// $messages = [
-			// 	'type' => 'text',
-			// 	'text' => $userId
-			// ];
+			$displayName = json_decode($row['name'], true);
+			$messages = [
+				'type' => 'text',
+				'text' => 'สวัสดี ' . $displayName['displayName']
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			// $url = 'https://api.line.me/v2/bot/message/reply';
-			// $data = [
-			// 	'replyToken' => $replyToken,
-			// 	'messages' => [$messages],
-			// ];
-			// $post = json_encode($data);
-			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			// $ch = curl_init($url);
-			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			// $result = curl_exec($ch);
-			// curl_close($ch);
-			// echo $result . "\r\n";
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 		}
 	}
 }
